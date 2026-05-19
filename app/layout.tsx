a6bc573d2d10027d7230b1a2ccecb94fb3741833
Your Name <you@example.com>
2026-05-19 19:05:36 +0700

    fix: update globals.css import path to absolute

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import '@/app/globals.css';
 import SpotlightCursor from "@/app/components/SpotligthCursor/SpotlightCursor";
 import Navbar from "@/app/components/Navbar/Navbar";
 import Header from "@/app/components/Header/Header";
